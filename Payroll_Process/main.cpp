1d6d54a6637270e5fad6c0f15c194219131cbcea
Carl Wainwright <carljame7@gmail.com>
2021-10-06 12:25:38 +0100

    Initial Commit

---
+++ Payroll_Process/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Payroll_Process
+//
+//  Created by Carl Wainwright on 06/10/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
